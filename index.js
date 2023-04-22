ad07a4020837c3fd7f3b2aa799bf169ad7c5573b
scattamooch <71458730+scattamooch@users.noreply.github.com>
2023-04-22 15:38:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sean",
+    streetAddress: "729 Cedar",
+};
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        name: "Sam",
+        streetAddress: "11 Broadway",
+    };
+} ;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = {employee}
+    delete newEmployee.name
+    return newEmployee
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name
+    return employee
+}
